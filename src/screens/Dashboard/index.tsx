b3e027ac34174d38b645249b1ee1cf78c6389e8e
Ricardo Lima <ricardo.engp@gmail.com>
2022-07-08 20:59:54 -0300

    feat: transactions list done

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,4 +1,9 @@
+import { getBottomSpace } from "react-native-iphone-x-helper";
 import { HighlightCard } from "../../components/HighlightCard";
+import {
+  TransactionCard,
+  TransactionCardProps,
+} from "../../components/TransactionCard";
 import {
   Container,
   Header,
@@ -10,9 +15,43 @@ import {
   UserName,
   UserWrapper,
   HighlightCards,
+  Transactions,
+  TransactionList,
+  Title,
 } from "./styles";
 
+export interface DataListProps extends TransactionCardProps {
+  id: string;
+}
+
 export const Dashboard = () => {
+  const data: DataListProps[] = [
+    {
+      id: "1",
+      type: "positive",
+      title: "Desenvolvimento de site",
+      amount: "R$ 12.000,00",
+      category: { name: "Vendas", icon: "dollar-sign" },
+      date: "13/04/2020",
+    },
+    {
+      id: "2",
+      type: "negative",
+      title: "Hamburgueria Pizzy",
+      amount: "R$ 59,00",
+      category: { name: "Alimentação", icon: "coffee" },
+      date: "13/04/2020",
+    },
+    {
+      id: "3",
+      type: "negative",
+      title: "Aluguel de apartamento",
+      amount: "R$ 1.200,00",
+      category: { name: "Casa", icon: "shopping-bag" },
+      date: "13/04/2020",
+    },
+  ];
+
   return (
     <Container>
       <Header>
@@ -51,6 +90,14 @@ export const Dashboard = () => {
           lastTransaction="1 à 16 de abril"
         />
       </HighlightCards>
+      <Transactions>
+        <Title>Listagem</Title>
+        <TransactionList
+          data={data}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <TransactionCard data={item} />}
+        />
+      </Transactions>
     </Container>
   );
 };
