a9e93f45d17a150bfdf036774fa3c377a8cf3c1a
Ricardo Lima <ricardo.engp@gmail.com>
2022-10-18 12:18:19 -0300

    feat: login screen created

--- App.tsx
+++ App.tsx
@@ -1,50 +1,45 @@
 import React from "react";
 import "intl";
 import "intl/locale-data/jsonp/pt-BR";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { ThemeProvider } from "styled-components";
+import {GestureHandlerRootView} from "react-native-gesture-handler";
+import {ThemeProvider} from "styled-components";
 import * as SplashScreen from "expo-splash-screen";
 
-import {
-  useFonts,
-  Poppins_400Regular,
-  Poppins_500Medium,
-  Poppins_700Bold,
-} from "@expo-google-fonts/poppins";
+import {Poppins_400Regular, Poppins_500Medium, Poppins_700Bold, useFonts,} from "@expo-google-fonts/poppins";
 
 import theme from "./src/global/styles/theme";
 
-import { NavigationContainer } from "@react-navigation/native";
+import {NavigationContainer} from "@react-navigation/native";
 
-import { StatusBar } from "react-native";
-import { Routes } from "./src/routes/app.routes";
+import {StatusBar} from "react-native";
+import {SignIn} from "./src/screens/SignIn";
 
 export default function App() {
-  SplashScreen.preventAutoHideAsync();
-  const [fontsLoaded] = useFonts({
-    Poppins_400Regular,
-    Poppins_500Medium,
-    Poppins_700Bold,
-  });
+    SplashScreen.preventAutoHideAsync();
+    const [fontsLoaded] = useFonts({
+        Poppins_400Regular,
+        Poppins_500Medium,
+        Poppins_700Bold,
+    });
 
-  if (!fontsLoaded) {
-    return null;
-  }
+    if (!fontsLoaded) {
+        return null;
+    }
 
-  SplashScreen.hideAsync();
+    SplashScreen.hideAsync();
 
-  return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <ThemeProvider theme={theme}>
-        <NavigationContainer>
-          <StatusBar
-            backgroundColor="transparent"
-            translucent
-            barStyle="light-content"
-          />
-          <Routes />
-        </NavigationContainer>
-      </ThemeProvider>
-    </GestureHandlerRootView>
-  );
+    return (
+        <GestureHandlerRootView style={{flex: 1}}>
+            <ThemeProvider theme={theme}>
+                <NavigationContainer>
+                    <StatusBar
+                        backgroundColor="transparent"
+                        translucent
+                        barStyle="light-content"
+                    />
+                    <SignIn/>
+                </NavigationContainer>
+            </ThemeProvider>
+        </GestureHandlerRootView>
+    );
 }
---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import LogoSvg from '../../assets/logo.svg';
+import GoogleSvg from '../../assets/google.svg';
+import AppleSvg from '../../assets/apple.svg';
+
+
+import {Container, Footer, FooterWrapper, Header, SignInTitle, Title, TitleWrapper} from './styles';
+import {RFValue} from "react-native-responsive-fontsize";
+import {SignInSocialButton} from "../../components/SignInSocialButton";
+
+export const SignIn = () => {
+    return (
+        <Container>
+            <Header>
+                <TitleWrapper>
+                    <LogoSvg width={RFValue(120)} height={RFValue(68)}/>
+                    <Title>Controle suas {'\n'} finanças de forma {'\n'} muito simples</Title>
+                </TitleWrapper>
+                <SignInTitle>Faça seu login com {'\n'} uma das contas abaixo</SignInTitle>
+            </Header>
+            <Footer>
+                <FooterWrapper>
+                    <SignInSocialButton title="Entrar com Google" svg={GoogleSvg}/>
+                    <SignInSocialButton title="Entrar com Apple" svg={AppleSvg}/>
+                </FooterWrapper>
+            </Footer>
+        </Container>
+    );
+}
